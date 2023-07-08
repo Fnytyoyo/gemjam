fac06b913a98aea817cbb8b9240f4e208a0e8065
Fnyt <fnytyoyo@gmail.com>
2023-07-08 12:37:10 +0200

    Ragdoll physics prototype

---
+++ Assets/Scripts/Ragdoll/BoneBalanceConfig.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BoneBalanceConfig", order = 1)]
+public class BoneBalanceConfig : ScriptableObject
+{
+    public float targetRotation = 0f;
+    public float getUpTime = 2.5f;
+    public float velocityThreshold = 2f;
+    public float maxForce = 75f;
+    public float getUpSpeed = 20f;
+}
