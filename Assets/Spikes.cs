9a1e00287ca3ace6cac23ef5a4f955960f8cd90d
Alek Płocharski <plocharskia@knur.mini.pw.edu.pl>
2023-07-09 10:59:55 +0200

    magnet added

--- Assets/Spikes.cs
+++ Assets/Spikes.cs
@@ -2,49 +2,52 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Spikes : ContraptionBase
 {
-    private Vector2 direction = Vector2.up;
-    private bool Active
-    {
-        get => transform.gameObject.activeSelf;
-        set => transform.gameObject.SetActive(value);
-    }
+    public float maxDistance;
+    public float forcePower;
 
+    private bool active = false;
+    
     private void Start()
     {
-        Active = false;
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void ApplyForce(Rigidbody2D limb, Vector2 forceDirection)
     {
-        if(!Active) return;
-        var body = other.transform.GetComponent<Rigidbody2D>();
-        body.velocity = Vector2.zero;
-        body.angularVelocity = 0f;
+        float distance = forceDirection.magnitude;
+        float normalizedDistance = distance / maxDistance;
+        float distanceModifier = -1 / (1 + Mathf.Exp(-30 * (normalizedDistance - 0.8f))) + 1;
+        
+        limb.AddForce(forceDirection.normalized * (forcePower * distanceModifier));
+    }
+    
+    private void ApplyForces()
+    {
+        foreach (var limb in Limbs.limbRigidBodies)
+        {
+            var direction3D = this.transform.position - limb.gameObject.transform.position;
+            var direction2D = new Vector2(direction3D.x, direction3D.y);
+            if (direction2D.magnitude < maxDistance)
+            {
+                ApplyForce(limb, direction2D);
+            }
+            
+        }
     }
     
     public override void OnInteract(Vector3 pos, int rotation)
     {
-        direction = rotation switch
+        active = !active;
+    }
+
+    private void FixedUpdate()
+    {
+        if (active)
         {
-            0 => Vector2.up,
-            1 => Vector2.right,
-            2 => Vector2.down,
-            3 => Vector2.left,
-            _ => direction
-        };
-        Vector3 direction3D = new Vector3(direction.x, direction.y, 0);
-        transform.rotation = Quaternion.FromToRotation(new Vector3(0, 1), direction3D);
-        if (Active)
-        {
-            transform.position -= new Vector3(direction.x, direction.y, 0);
+            ApplyForces();
         }
-        else
-        {
-            transform.position += new Vector3(direction.x, direction.y, 0);
-        }
-        Active = !Active;
     }
 }
