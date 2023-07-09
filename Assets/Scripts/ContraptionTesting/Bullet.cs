2bb395320cce2b6ef2441502bc68a8f3bed61c03
Tomasz Herman <tomasz.herman@protonmail.com>
2023-07-09 15:03:26 +0200

    Fix bullet hit and add sound effect on hit

--- Assets/Scripts/ContraptionTesting/Bullet.cs
+++ Assets/Scripts/ContraptionTesting/Bullet.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float force = 40.0f;
+    public AudioSource audioSource;
+    public SpriteRenderer spriteRenderer;
+    public bool hit = false;
     
+    public float force = 40.0f;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
     void Update()
     {
         transform.rotation = Quaternion.FromToRotation(Vector2.right, GetComponent<Rigidbody2D>().velocity);
+
+        if (hit && !audioSource.isPlaying)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        if (hit) return;
         Vector2 direction = GetComponent<Rigidbody2D>().velocity.normalized;
         Vector2 position = col.transform.position;
         if (col.gameObject.layer == LayerMask.NameToLayer("Ragdoll"))
         {
             col.gameObject.GetComponent<Rigidbody2D>().AddForceAtPosition(direction * force, position, ForceMode2D.Impulse);
         }
-        
-        Destroy(this.gameObject);
+
+        hit = true;
+        spriteRenderer.enabled = false;
+        audioSource.Play();
     }
 }
